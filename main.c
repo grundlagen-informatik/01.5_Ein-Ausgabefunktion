cd84c7b12bc26ea9d52e3fb444c168b56a0a5de1
isabelge <isabel.geissmann@unibas.ch>
2023-09-08 18:03:29 +0200

    add solution

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main() {
+    int zahl1;
+    int zahl2;
+    int resultat;
 
     // Hier muss der Code für den Additions-Rechner geschrieben werden
+    printf("Please enter the first number: ");
+    scanf_s("%d", &zahl1);
+    printf("Please enter the second number: ");
+    scanf_s("%d", &zahl2);
+    resultat = zahl1 + zahl2;
+    printf("The result is = %d", resultat);
 
     return 0;
 }
